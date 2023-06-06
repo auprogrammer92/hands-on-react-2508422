5593b62b317b87e02e4e108e1256f1f22ec5afe7
Alan West <west.t.alan@gmail.com>
2023-06-06 10:46:33 -0500

    hopefully this works

--- src/components/Support.jsx
+++ src/components/Support.jsx
@@ -3,8 +3,20 @@ import { useState } from 'react';
 export default () => {
   const [count, setCount] = useState(0)
   return (
+    <>
+    <button className="outline" onClick={() => setCount(count + 1)}>
+      {(() => {
+        if (count === 0) {
+          return "Click to support";
+        } else {
+          return `Supported ${count} times`
+        }
+      })()}
+    </button>
+
     <button className="outline" onClick={() => setCount(count + 1)}>
       {count === 0 ? "Click to support": `Supported ${count} times`}
     </button>
+    </>
   )
 }
